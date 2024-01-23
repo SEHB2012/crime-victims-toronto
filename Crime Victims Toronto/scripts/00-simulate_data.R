02c5afa1050fbdbe98e87ad950f7c531e253413c
SEHB2012 <sehar.bajwa@mail.utoronto.ca>
2024-01-23 23:44:26 +0000

    setting up workplace

--- Crime Victims Toronto/scripts/00-simulate_data.R
+++ Crime Victims Toronto/scripts/00-simulate_data.R
@@ -1,14 +1,20 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates a dataset regarding crimes in Toronto ... [...UPDATE THIS...]
+# Author: Sehar Bajwa
+# Date: 23 January 2023 
+# Contact: sehar@mail.utoronto.ca
 
 
 #### Workspace setup ####
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("opendatatoronto")
+install.packages("knitr")
+
+library(knitr)
+library(janitor)
+library(lubridate)
+library(opendatatoronto)
 library(tidyverse)
 # [...UPDATE THIS...]
 
