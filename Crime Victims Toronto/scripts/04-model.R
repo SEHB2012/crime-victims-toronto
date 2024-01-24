bf56ce2042cabd37e8c40f224be05fe7bca67f3b
SEHB2012 <sehar.bajwa@mail.utoronto.ca>
2024-01-24 06:23:13 +0000

    graphs

--- Crime Victims Toronto/scripts/04-model.R
+++ Crime Victims Toronto/scripts/04-model.R
@@ -1,37 +1,50 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Purpose: Cleans up the dataset for use
+# Author: Sehar Bajwa 
+# Date: 23 January 2023 
+# Contact: sehar.bajwa@mail.utoronto.ca
 
 #### Workspace setup ####
-library(tidyverse)
-library(rstanarm)
+#install.packages("pheatmap")
+library("pheatmap")
 
-#### Read data ####
-analysis_data <- read_csv("outputs/data/analysis_data.csv")
+### Data Model 1###
+# Scatterplot showing changing in number of crimes per year per crime type
+data_groupsbycrime |>
+  ggplot(aes(x = report_year, y = sum_count, color = subtype, type ="b")) +
+  geom_point() +
+  geom_line()+
+  theme_minimal() +
+  labs(x = "Year", y = "Crime Count", color = "Crime Type",type ="b") +
+  scale_color_brewer(palette = "Set1") +
+  ggtitle("Trends in crimes against females in Toronto")
+theme(legend.position = "right")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+### Data Model 2###
+# heatmap using crime count as color intensity to show which age groups are most susceptible to crime 
 
+# Custom order vector
+custom_order <- c("<12", "12 to 17", "18 to 24", "25 to 34","35 to 44","45 to 54","55 to 64","65+")
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "outputs/models/first_model.rds"
+data_groupsbyage <- data_groupsbyage %>%
+  arrange(factor(age_cohort, levels = custom_order))
+# Pivot the data to wide format for the heatmap
+heatmap_data <- data_groupsbyage %>%
+  pivot_wider(names_from = report_year, values_from = sum_count, values_fill = 0)
+str(heatmap_data)
+heatmap_matrix <- as.matrix(heatmap_data[1:nrow(heatmap_data), -1])
+
+#using package pretty heatmap
+pheatmap(heatmap_matrix,
+         display_numbers = T,
+         Colv = NA,
+         Rowv = NA,
+         show_rownames = T,
+         show_colnames = T,
+         xlab = "Year",
+         ylab = "Crime Type",
+         main = "Which female age group is most susceptible to crimes in Toronto?",
+         labels_row=c("<12","12 to 17","18 to 24","25 to 34","35 to 44","45 to 54","55 to 64","65+"),
+         cluster_cols = F,
+         cluster_rows = F,
 )
-
-
